503cce1fb7ca27b36ad0467f0fcabe3777914133
holden <james.holden.moore@gmail.com>
2026-03-08 17:06:58 -0400

    add link info to constants.js

---
+++ constants.js
@@ -0,0 +1,61 @@
+// Mother Meera - Social Media Links Configuration
+// Update this file to manage all social media and external links
+
+export const LINKS = [
+  {
+    id: "website",
+    label: "Official Website",
+    url: "https://www.mothermeera.com",
+    icon: "fa-globe",
+    brand: "website",
+  },
+  {
+    id: "youtube",
+    label: "YouTube",
+    url: "https://www.youtube.com/@MotherMeeraOfficial",
+    icon: "fa-brands fa-youtube",
+    brand: "youtube",
+  },
+  {
+    id: "facebook",
+    label: "Facebook",
+    url: "https://www.facebook.com/mothermeera",
+    icon: "fa-brands fa-facebook-f",
+    brand: "facebook",
+  },
+  {
+    id: "instagram",
+    label: "Instagram",
+    url: "https://www.instagram.com/mothermeeraofficial",
+    icon: "fa-brands fa-instagram",
+    brand: "instagram",
+  },
+  {
+    id: "twitter",
+    label: "X / Twitter",
+    url: "https://x.com/MotherMeeraOrg",
+    icon: "fa-brands fa-x-twitter",
+    brand: "twitter",
+  },
+  {
+    id: "spotify",
+    label: "Spotify – Sacred Music",
+    url: "https://open.spotify.com/artist/6YpQu19Ar6T8m1VHkxT5oo",
+    icon: "fa-brands fa-spotify",
+    brand: "spotify",
+  },
+];
+
+export const PROFILE = {
+  name: "Mother Meera",
+  tagline: "Divine Mother · Spiritual Teacher",
+  avatarUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Mother_Meera.jpg/320px-Mother_Meera.jpg",
+  avatarAlt: "Mother Meera",
+};
+
+export const SITE = {
+  title: "Mother Meera",
+  description: "Mother Meera – Official Social Media Links",
+  copyrightName: "Mother Meera",
+};
